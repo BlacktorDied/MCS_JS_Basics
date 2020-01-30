482952d2852f73eeef1efef46e34aaff1ea5c0be
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-01-31 00:17:57 +0200

    scripts

---
+++ Test_scripts/1_script.js
@@ -0,0 +1,1 @@
+document.body.innerHTML = (prompt("Enter HTML-code"))
---
+++ Test_scripts/3_script.js
@@ -0,0 +1,7 @@
+let money = prompt('How much money you have?')
+let apples = prompt('How many apples you want to buy?')
+let bread = prompt('How many loaves of bread you want to buy?')
+let apple = prompt('how much does one apple cost?')
+let bread2 = prompt('How much does one loaf of bread cost?')
+let total = (money-(apples*apple)-(bread*bread2))
+document.body.innerText = (total>=0)
