7b8ca084010c117d8035789229f558ad94fa2a1c
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-02-11 18:53:21 +0200

    practice_04_fix

--- Practice_04/js/script.js
+++ Practice_04/js/script.js
@@ -3,8 +3,8 @@ console.log('You are at'+window.location);
 const answer = document.querySelector('.number');
 let number = prompt('Write here number');
 
-if(number<=0) {
+if(number>=0) {
 	console.log(number)
 }else {
-	console.log(number)
+	console.log(- number)
 }
