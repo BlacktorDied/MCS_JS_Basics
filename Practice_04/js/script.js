8b4e1e3a2d9a2e34a9fc6b1f0255a5910297ac1d
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-02-11 18:46:58 +0200

    practice 04

---
+++ Practice_04/js/script.js
@@ -0,0 +1,10 @@
+console.log('You are at'+window.location);
+
+const answer = document.querySelector('.number');
+let number = prompt('Write here number');
+
+if(number<=0) {
+	console.log(number)
+}else {
+	console.log(number)
+}
