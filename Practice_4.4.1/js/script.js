95cb27abf26e6ab46e262faa8c4caf1b960295d2
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-02-17 12:40:18 +0200

    practice_4.4.1

---
+++ Practice_4.4.1/js/script.js
@@ -0,0 +1,12 @@
+console.log('You are at'+window.location);
+
+const answer = document.querySelector('.number');
+let number = prompt('Write here number');
+
+/*if(number>=0) {
+	console.log(number)
+}else {
+	console.log(- number)
+}*/
+
+(number >= 0) ? console.log(number) : console.log(- number)
