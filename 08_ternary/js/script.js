6b7db9461101a0ba7a950f53698de28da9b443df
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-02-17 12:32:14 +0200

    ternary_fix

--- 08_ternary/js/script.js
+++ 08_ternary/js/script.js
@@ -4,12 +4,4 @@ const image = document.querySelector('.imageBox');
 let answer = prompt('Сколько тебе лет?');
 answer = parseInt(answer);
 
-if(answer<18) {
-	image.style['background-image'] = 'url(img/school.png)';
-}else if(answer<=25) {
-	image.style['background-image'] = 'url(img/party.jpg)';
-}else if(answer<=50) {
-	image.style['background-image'] = 'url(img/pub.png)';
-}else {
-	image.style['background-image'] = 'url(img/home.png)';
-}
+(answer >= 18) ? image.style.backgroundImage = 'url(img/pub.png)' : image.style.backgroundImage = 'url(img/school.png)';
