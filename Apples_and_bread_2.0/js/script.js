493d936cd246a51d7029078ba46cb88a2843e08f
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-02-23 19:32:52 +0200

    A&B_2.0

---
+++ Apples_and_bread_2.0/js/script.js
@@ -0,0 +1,13 @@
+let money = prompt('How much money you have?');
+let apples = prompt('How many apples you want to buy?');
+let bread = prompt('How many loaves of bread you want to buy?');
+let apple = prompt('how much does one apple cost?');
+let bread2 = prompt('How much does one loaf of bread cost?');
+
+function haveEnough(money, apples, bread, apple, bread2){
+	let total = (money-(apples*apple)-(bread*bread2));
+	document.body.innerText = (total>=0);
+	(total >= 18) ? console.log('You have enough') : console.log('You haven`t got enough');	
+}
+
+haveEnough(money, apples, bread, apple, bread2);
