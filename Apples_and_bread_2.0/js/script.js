b4d342d1acd73f0175f17edea2df4cab6f99ddb8
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-02-25 21:13:24 +0200

    A&B_2.0_fix

--- Apples_and_bread_2.0/js/script.js
+++ Apples_and_bread_2.0/js/script.js
@@ -1,11 +1,12 @@
-let money = prompt('How much money you have?');
-let apples = prompt('How many apples you want to buy?');
-let bread = prompt('How many loaves of bread you want to buy?');
-let apple = prompt('how much does one apple cost?');
-let bread2 = prompt('How much does one loaf of bread cost?');
+let money = parseFloat(prompt('How much money you have?'));
+let apples = parseFloat(prompt('How many apples you want to buy?'));
+let bread = parseFloat(prompt('How many loaves of bread you want to buy?'));
+let apple = parseFloat(prompt('how much does one apple cost?'));
+let bread2 = parseFloat(prompt('How much does one loaf of bread cost?'));
+let total = parseFloat((money-(apples*apple)-(bread*bread2)));
 
 function haveEnough(money, apples, bread, apple, bread2){
-	let total = (money-(apples*apple)-(bread*bread2));
+	let total = parseFloat((money-(apples*apple)-(bread*bread2)));
 	document.body.innerText = (total>=0);
 	(total >= 0) ? console.log('You have enough') : console.log('You haven`t got enough');	
 }
