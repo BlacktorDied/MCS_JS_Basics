d5632cd4fb69f6f9f3a16177e4326bb818a75f3f
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-03-19 20:51:30 +0200

    array&object

---
+++ 21_array&object/js/script.js
@@ -0,0 +1,13 @@
+console.log('You are at'+window.location);
+
+const cools0 = document.getElementsByClassName('cool');
+const cools1 = document.getElementsByTagName('div');
+const cools2 = document.querySelectorAll('.cool');
+
+console.log(cools0[0]);
+console.log(cools1[1]);
+console.log(cools2[2]);
+
+/*cools0[0].innerHTML = 'I am first';
+cools1[1].innerHTML = 'I am second';
+cools2[2].innerHTML = 'I am third';*/
