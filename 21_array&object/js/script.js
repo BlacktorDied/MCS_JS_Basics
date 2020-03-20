74cb7acf2bf19f4fc72aee19982cef1fb547c249
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-03-20 11:08:18 +0200

    array&object

--- 21_array&object/js/script.js
+++ 21_array&object/js/script.js
@@ -8,6 +8,6 @@ console.log(cools0[0]);
 console.log(cools1[1]);
 console.log(cools2[2]);
 
-/*cools0[0].innerHTML = 'I am first';
-cools1[1].innerHTML = 'I am second';
+cools0[0].innerHTML = 'I am first';
+/*cools1[1].innerHTML = 'I am second';
 cools2[2].innerHTML = 'I am third';*/
