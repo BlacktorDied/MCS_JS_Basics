f9317fc4b3b61f01bee470460fd661e3eb2a2c5b
BlacktorDied <55595377+BlacktorDied@users.noreply.github.com>
2020-04-25 11:28:22 +0300

    Practice_5.11.1

---
+++ Practice_5.11.1/js/script.js
@@ -0,0 +1,25 @@
+console.log('You are at'+window.location);
+
+let names = [
+[['куры','гуси','павлины'],
+['сокол', 'орел', 'соловей']],
+[['собака', 'кошка'],
+['обезьяна', 'рысь']]];
+
+for(let i=0; i<names[0][0].length; i++){
+	console.log(names[0][0][i]);
+}
+
+for(name of names[0][1]){
+	console.log(name);
+}
+
+for(key in names[1][0]){
+	console.log(names[1][0][key]);
+}
+
+window.onload = () => {
+	names[1][1].forEach((name) =>{
+		console.log(name)
+	})
+}
